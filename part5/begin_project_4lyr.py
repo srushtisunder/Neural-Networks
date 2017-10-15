bf7a415fb9ad2793911964576a390e526b1a90df
Deepansh Pahwa <deepanshpahwa96@gmail.com>
2017-10-15 20:09:31 +0530

    All parts done| Documentation Left

--- part5/begin_project_4lyr.py
+++ part5/begin_project_4lyr.py
@@ -2,7 +2,6 @@ import numpy as np
 import theano
 import theano.tensor as T
 import matplotlib.pyplot as plt
-import timeit
 
 def init_bias(n = 1):
     return(theano.shared(np.zeros(n), theano.config.floatX))
@@ -39,18 +38,34 @@ def shuffle_data (samples, labels):
     return samples, labels
 
 
-decay = 1e-9
+decay = 1e-6
 learning_rate = 0.01
 epochs = 1000
 batch_size = 32
-neuronArray=[5,10,15,20,25]
-numberOfNeuron=10
-timeTakenArray=[]
-batchArray=[4,8,16,32,64]
+numberOfNeuron1=10
+numberOfNeuron2=10
 
 # theano expressions
 X = T.matrix() #features
 Y = T.matrix() #output
+
+w1, b1 = init_weights(36, numberOfNeuron1), init_bias(numberOfNeuron1) #weights and biases from input to hidden layer
+w2 ,b2 = init_weights(numberOfNeuron1, numberOfNeuron2, logistic=False), init_bias(numberOfNeuron2)
+w3, b3 = init_weights(numberOfNeuron2, 6, logistic=False), init_bias(6) #weights and biases from hidden to output layer
+
+h1 = T.nnet.sigmoid(T.dot(X, w1) + b1)
+h2 = T.nnet.sigmoid(T.dot(h1, w2) + b2)
+py = T.nnet.softmax(T.dot(h2, w3) + b3)
+
+y_x = T.argmax(py, axis=1)
+
+cost = T.mean(T.nnet.categorical_crossentropy(py, Y)) + decay*(T.sum(T.sqr(w1)+T.sum(T.sqr(w2))+T.sum(T.sqr(w3))))
+params = [w1, b1, w2, b2, w3, b3]
+updates = sgd(cost, params, learning_rate)
+
+# compile
+train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
+predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
 
 
 #read train data
@@ -78,27 +93,6 @@ testY[np.arange(test_Y.shape[0]), test_Y-1] = 1
 print(trainX.shape, trainY.shape)
 print(testX.shape, testY.shape)
 
-
-#for numberOfNeuron in neuronArray:
-
-w1, b1 = init_weights(36, numberOfNeuron), init_bias(numberOfNeuron) #weights and biases from input to hidden layer
-w2, b2 = init_weights(numberOfNeuron, 6, logistic=False), init_bias(6) #weights and biases from hidden to output layer
-
-h1 = T.nnet.sigmoid(T.dot(X, w1) + b1)
-py = T.nnet.softmax(T.dot(h1, w2) + b2)
-
-y_x = T.argmax(py, axis=1)
-
-cost = T.mean(T.nnet.categorical_crossentropy(py, Y)) + decay*(T.sum(T.sqr(w1)+T.sum(T.sqr(w2))))
-params = [w1, b1, w2, b2]
-updates = sgd(cost, params, learning_rate)
-
-# compile
-train = theano.function(inputs=[X, Y], outputs=cost, updates=updates, allow_input_downcast=True)
-predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
-
-
-
 # first, experiment with a small sample of data
 ##trainX = trainX[:1000]
 ##trainY = trainY[:1000]
@@ -110,9 +104,6 @@ predict = theano.function(inputs=[X], outputs=y_x, allow_input_downcast=True)
 n = len(trainX)
 test_accuracy = []
 train_cost = []
-#for batch_size in batchArray:
-start = timeit.default_timer()
-
 for i in range(epochs):
     if i % 1000 == 0:
         print(i)
@@ -125,42 +116,21 @@ for i in range(epochs):
 
     test_accuracy = np.append(test_accuracy, np.mean(np.argmax(testY, axis=1) == predict(testX)))
 
-end= timeit.default_timer()
-timeTakenArray.append((end-start)/batch_size)
-
-
 print('%.1f accuracy at %d iterations'%(np.max(test_accuracy)*100, np.argmax(test_accuracy)+1))
-
 
 #Plots
 plt.figure()
 plt.plot(range(epochs), train_cost)
 plt.xlabel('iterations')
 plt.ylabel('cross-entropy')
-plt.title('training cost | bs=32 | N=10 | decay=1e-9')
-plt.savefig('p1a_q4a_sample_cost_decay=1e-9.png')
+plt.title('training cost')
+plt.savefig('p1a_sample_cost.png')
 
 plt.figure()
 plt.plot(range(epochs), test_accuracy)
 plt.xlabel('iterations')
 plt.ylabel('accuracy')
-plt.title('test accuracy | bs=32 | N=10 | decay=1e-9')
-plt.savefig('p1a_q4b_sample_accuracy_decay=1e-9.png')
-'''
-#for P1A Q2b
-plt.figure()
-plt.plot(batchArray, timeTakenArray)
-plt.xlabel('batchArray')
-plt.ylabel('timeTakenArray')
-plt.title('parameter update time')
-plt.savefig('p1a_parameter_update_time.png')
+plt.title('test accuracy')
+plt.savefig('p1a_sample_accuracy.png')
 
-#for P1a Q3b_updatetime_vs_numberofneurons
-plt.figure()
-plt.plot(neuronArray, timeTakenArray)
-plt.xlabel('Number Of Neurons')
-plt.ylabel('Time Taken')
-plt.title('parameter update time')
-plt.savefig('p1a_q3b_parameter_update_time.png')
-'''
 #plt.show()
